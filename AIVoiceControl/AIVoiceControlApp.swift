7903a98b61059574e5206d22ddac139551a16c88
Jack Kim <jack.kim.dev@gmail.com>
2025-07-30 23:45:38 +0900

    Initial commit

---
+++ AIVoiceControl/AIVoiceControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIVoiceControlApp.swift
+//  AIVoiceControl
+//
+//  Created by Jack Kim on 7/30/25.
+//
+
+import SwiftUI
+
+@main
+struct AIVoiceControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AIVoiceControlTests/AIVoiceControlTests.swift
@@ -0,0 +1,17 @@
+//
+//  AIVoiceControlTests.swift
+//  AIVoiceControlTests
+//
+//  Created by Jack Kim on 7/30/25.
+//
+
+import Testing
+@testable import AIVoiceControl
+
+struct AIVoiceControlTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
